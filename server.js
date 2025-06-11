879c22df84e8d23721451eee8ba808a19f4782dc
Zoe Nguyen <nganguyen0703@gmail.com>
2025-06-11 15:36:51 +0800

    feat: add connection to mongoDB

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 
+const mongoose = require("mongoose");
 const express = require("express");
 const cors = require("cors");
 
@@ -11,6 +12,13 @@ const port = process.env.PORT || 8800;
 // in .env file add CORS_ORIGIN=*
 app.use(cors({ origin: process.env.CORS_ORIGIN }));
 
-app.listen(port, () => {
-	console.log(`====>XXX app listening on port ${port}<====`);
-});
+app.listen(port, async() => {
+  try {
+    await mongoose.connect(process.env.MONGO_DB_STRING, { dbName: "XPeditr" });
+  } catch (error) {
+    console.log(`====>Failed to connect to DB<==== Error: ${error}`);
+    process.exit(1);
+  }
+  console.log(`====>Connected to MongoDB`);
+  console.log(`====>XPeditr app listening on port ${port}<====`);
+})
