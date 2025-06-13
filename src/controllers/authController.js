9b6ef2ef284388cf0c287ca5eb35754071985b8a
Zoe Nguyen <nganguyen0703@gmail.com>
2025-06-13 11:13:36 +0800

    feat: add check for pending user account in register function

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -22,12 +22,16 @@ const controller = {
 
       // check if there is an existing account with the same email and/or username
       const [emailExists, usernameExists] = await Promise.all([
-        UserModel.findOne({ normalizedEmail }),
+        UserModel.findOne({ email: normalizedEmail }),
         UserModel.findOne({ username }),
       ]);
 
       if (emailExists) throw badRequest("An account with this email already exists");
       if (usernameExists) throw badRequest("An account with this username already exists");
+
+      // check if there is a pending registration with the same email
+      const pendingUser = await PendingUserModel.findOne({ email: normalizedEmail});
+      if (pendingUser) throw badRequest("This email has been used for registration before. Please check your email inbox for activation link.");
 
       const passwordHash = await bcrypt.hash(password, 10);
       const expiresAt = new Date(Date.now() + 15 * 60 * 1000); // 15 minutes
